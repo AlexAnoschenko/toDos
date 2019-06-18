37548080dfff58d376bc461dafc315c6870ba340
Alexey Anoschenko <elegalex@ya.ru>
2019-06-18 17:53:58 +0300

    Added delete function and refactoring check method

--- script/script.js
+++ script/script.js
@@ -4,6 +4,8 @@ var dateTask = document.getElementById("date-task");
 var blockForTasks = document.getElementById("block-for-tasks");
 
 addButton.addEventListener("click", addNewTask);
+blockForTasks.addEventListener("click", removeTask);
+blockForTasks.addEventListener("click", taskIsDone);
 
 function addNewTask() {
   var newInputedTask = mainInput.value;
@@ -16,24 +18,37 @@ function addNewTask() {
 
     var nextTask = document.createElement("span");
     var nextDate = document.createElement("span");
+    var closeButton = document.createElement("span");
 
     nextTask.innerHTML = newInputedTask;
     nextDate.innerHTML = newInputedDate;
+    closeButton.innerHTML = "<b>[X]<b>";
+    nextTask.classList.add("styleTask");
+    nextDate.classList.add("styleDate");
+    closeButton.classList.add("close-button");
 
     newDiv.appendChild(nextTask);
     newDiv.appendChild(nextDate);
+    newDiv.appendChild(closeButton);
   } else {
     return;
   }
 }
 
-blockForTasks.addEventListener("click", taskIsDone);
+function removeTask(event) {
+  var target = event.target;
+  if (target.tagName == "B") {
+    target.parentNode.parentNode.remove();
+  } else {
+    return;
+  }
+}
 
 function taskIsDone(event) {
-    var target = event.target;
-    if (target.tagName == "SPAN") {
-        target.parentNode.classList.add("checked");
-    } else {
-        return;
-    }
-};
+  var target = event.target;
+  if (target.tagName == "SPAN") {
+    target.parentNode.classList.add("checked");
+  } else {
+    return;
+  }
+}
